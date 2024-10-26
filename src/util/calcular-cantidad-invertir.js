458cd81390501b514913cf52c1e41bfe68cb74ef
Fabio Flores <efabiofm@gmail.com>
2024-10-25 18:39:20 -0600

    mostrar el tamaño de orden en USDT

--- src/util/calcular-cantidad-invertir.js
+++ src/util/calcular-cantidad-invertir.js
@@ -4,5 +4,5 @@ const riesgoPorTrade = capitalTotal * 0.01; // 1% del capital
 export default function calcularCantidadInvertir(entrada, stopLoss) {
   const riesgoPorUnidad = Math.abs(entrada - stopLoss);
   const cantidad = riesgoPorTrade / riesgoPorUnidad;
-  return cantidad;
+  return cantidad * entrada;
 }
